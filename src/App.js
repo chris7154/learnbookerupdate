e89f432884a06d99c49d0ea52a068dff18b04ecf
Christopher Njoku <chrisnjoku95@gmail.com>
2023-09-06 02:49:53 +0100

    learn booker updated project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import './App.css';
-import Header from "./components/common/header/Header"
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Home from './components/home/Home'
-import About from "./components/about/About"
+import React from 'react';
+import './App.css'
+import Navbar from './components/Navbar';
 
-
-
-
+import { BrowserRouter as Router} from 'react-router-dom'
 
 function App () {
   return (
     <>
       
       <Router>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route path="/About" element={<About/>} />
-        </Routes>
+        <Navbar />
       </Router>
     </>
   );
